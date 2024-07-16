cba20ffccd03ba30a1a9372a1a0718452fc63bfc
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-07-16 12:16:14 +0100

    configurando cores no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily:{
+      sans: 'Roboto'
+    },
+    extend: {
+      colors: {
+        'custom-green': '#10B981',
+        'custom-blue': '#0EA5E9',
+        'custom-gray': '#6B7280',
+        'custom-b-gray': '#94A3B8',
+        'custom-text-button-color': '#F1F5F9'
+      },
+    },
   },
   plugins: [],
 }
