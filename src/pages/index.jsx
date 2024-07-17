1a4fdb4c8563045f72c14c28e29e85d19885a56f
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-07-17 20:22:18 +0100

    Criação das rotas para interface de signIn e signUp

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { Home } from "./Home";
 import { NotFound } from "./NotFound";
+import { SignUp } from "./SignUp";
+import { SignIn } from "./SignIn";
 
 export const Pages = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route exact path='/sign-up' element={<SignUp />} />
+      <Route path='/sign-in' element={<SignIn />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
