c00e4aadd619dad8c68fbd9c3cb5953b71ce07f2
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-07-18 11:13:24 +0100

    fix: inetrface home e card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import { Button } from "../components/button";
 import { CardPost } from "../components/card-post";
 import { App } from "../layouts/App";
-
-
-
+import { useState } from "react";
 
 export const Home = () => {
+  const [message, setMessage] = useState("");
+
+  const handleLogout = () => {
+    console.log("SAINDO DA APLICACAO");
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("PUBLICANDO UM NOVO TWEET");
+  };
 
   return (
     <App>
@@ -14,45 +22,52 @@ export const Home = () => {
           <p className="text-custom-blue cursor-pointer">aluritter</p>
         </div>
         <div className="flex items-center gap-3">
-          <p className="font-normal text-custom-gray text-center">johndoe@example.com</p>
-          <Button
-            color={"red"}
-            padding={"sm"}
-            onClick={() => {
-              console.log("Saindo...");
-            }}
-          >
+          <p className="font-normal text-custom-gray text-center">
+            johndoe@example.com
+          </p>
+          <Button color={"red"} padding={"sm"} onClick={handleLogout}>
             Sair
           </Button>
         </div>
       </header>
-      <main className="mx-16 max-sm:mx-2 max-lg:mx-5">
-          <div className="flex flex-col gap-4">
-            <form className="flex flex-col gap-4">
-              <label htmlFor="post" className="">Alurite agora mesmo...</label>
-              <textarea
-                name="post"
-                rows="4"
-                cols="50" 
-                id=""
-                className="rounded-md border border-custom-b-gray resize-none"
-              ></textarea>
-              <div className="flex items-start justify-between">
-                <p className="text-green-500">Você ainda pode digitar 255 caracteres</p>
-                <Button
-                  color={"blue"}
-                  padding={"sm"}
-                  onClick={() => {
-                    console.log("Publicando...");
-                  }}
-                >
-                  Aluritar
-                </Button>
-              </div>
-            </form>
-            <CardPost />
-          </div>
+      <main className="mx-16 max-sm:mx-2 max-lg:mx-5 my-16">
+        <div className="flex flex-col gap-4">
+          <form onSubmit={(event)=> handleFormSubmit(event)} className="flex flex-col gap-4">
+            <label htmlFor="post" className="text-custom-gray">
+              Alurite agora mesmo...
+            </label>
+            <textarea
+              name="post"
+              rows="4"
+              cols="50"
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+              className="rounded-md border border-custom-b-gray resize-none"
+            ></textarea>
+            <div className="flex items-start justify-between">
+              {message.length < 255 ? (
+                <p className="text-sm text-green-600">
+                  Você ainda pode digitar {255 - message.length} caracteres
+                </p>
+              ) : (
+                <p className="text-sm text-red-600">
+                  Você esgotou a quantidade de caracteres
+                </p>
+              )}
+              <Button
+                color={"blue"}
+                padding={"sm"}
+                onClick={() => {
+                  console.log("Publicando...");
+                }}
+              >
+                Aluritar
+              </Button>
+            </div>
+          </form>
+          <CardPost />
+        </div>
       </main>
     </App>
   );
-}
+};
