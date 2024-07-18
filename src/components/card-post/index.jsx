cab3e8b0cce0096bdd70345727e01250645e8cb6
Emanuel-Marques <70699733+Emanuel-Marques@users.noreply.github.com>
2024-07-18 10:25:20 +0100

    componente card-post

---
+++ src/components/card-post/index.jsx
@@ -0,0 +1,14 @@
+export function CardPost() {
+  return (
+    <div className="w-full bg-slate-50 rounded-md border border-custom-b-gray py-4 px-4 flex flex-col justify-between gap-8">
+      <p className="text-base text-custom-gray">Seven7OfCode com React =DDD</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim urna, egestas non rhoncus a, imperdiet nec urna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas sed porttitor lorem, nec commodo nibh. Phasellus tincidunt feugiat erat quis sagittis. Pellentesque tincidunt suscipit fermentum. Ut pulvinar rhoncus libero porta sollicitudin. Donec vel ultrices sem. Donec bibendum dignissim orci, nec varius nunc auctor in. Sed vestibulum rutrum ligula, eget imperdiet lorem scelerisque ac. Nulla lorem velit, molestie eu mollis sed, finibus a leo. Sed sed tellus nisi. Suspendisse tempor ipsum risus, eget aliquet ligula dignissim ut. Quisque lorem metus, pulvinar et orci vitae, accumsan semper orci. Phasellus risus metus, tincidunt in viverra vitae, molestie id erat.
+
+Proin ac hendrerit dui. Fusce id purus lacus. Mauris lorem purus, dictum sed justo a, ullamcorper aliquam est. Nulla facilisis urna in odio dignissim pretium. Aliquam blandit lorem in fringilla tincidunt. Donec ultrices sem at faucibus finibus. Fusce vitae enim quis massa porta dictum. Integer ornare nibh pretium convallis pulvinar. Curabitur molestie ante sem, eu placerat risus ullamcorper eget. Nunc a porta ex. Etiam tincidunt, diam sit amet aliquet fermentum, augue lacus pulvinar ex, eu auctor ante est vitae ipsum. Quisque eleifend, velit ac sagittis blandit, mi dolor fermentum mi, non pharetra justo elit vel est. Nullam pulvinar dignissim velit vitae venenatis. Nulla pretium, neque dapibus ultrices porttitor, nisi enim facilisis lacus, et tristique libero eros at magna. Morbi eleifend vitae turpis in mollis. Aliquam pretium, mi non dignissim accumsan, diam eros varius lorem, eget commodo augue dolor quis velit.</p>
+      <div className="flex justify-between max-sm:flex-wrap">
+        <span className="text-custom-blue">johnfoe@example.com</span>
+        <span className="text-custom-b-gray">6/30/2023, 3:52:49 PM</span>
+      </div>
+    </div>
+  );
+}
